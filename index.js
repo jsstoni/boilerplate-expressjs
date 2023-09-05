8993c6c109debe40422a1d05b9d745d1125d7472
jsstoni <jsstoniha@gmail.com>
2023-09-04 21:37:46 -0300

    middleware errors

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const cors = require("cors");
 const helmet = require("helmet");
 const express = require("express");
+const {
+  logErrors,
+  wrapErrors,
+  errorHandle,
+} = require("./src/utils/middlewares/errorMiddleware");
 const config = require("./config");
 const port = config.port;
 const app = express();
@@ -9,5 +14,10 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(helmet());
+
+//middlewares
+app.use(logErrors);
+app.use(wrapErrors);
+app.use(errorHandle);
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
---
+++ src/utils/middlewares/errorMiddleware.js
@@ -0,0 +1,37 @@
+const config = require("../../../config");
+
+const withErrorStack = (error, stack) => {
+  if (config.dev) {
+    return { ...error, stack };
+  }
+  return error;
+};
+
+const logErrors = (err, req, res, next) => {
+  console.error(err);
+  next(err);
+};
+
+const wrapErrors = (err, req, res, next) => {
+  const outError = {
+    stack: err.stack,
+    output: {
+      statusCode: err.status,
+      payload: {
+        error: err.message || "Internal server error",
+      },
+    },
+  };
+  next(outError);
+};
+
+const errorHandle = (err, req, res, next) => {
+  const { stack, output } = err;
+  res.status(output.statusCode).json(withErrorStack(output.payload, stack));
+};
+
+module.exports = {
+  logErrors,
+  wrapErrors,
+  errorHandle,
+};
