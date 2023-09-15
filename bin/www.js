a150874d763cac7c4b2c959c270fe2dd378c3c91
jsstoni <jsstoniha@gmail.com>
2023-09-15 16:31:04 -0300

    server

---
+++ bin/www.js
@@ -0,0 +1,24 @@
+import http from "http";
+import app from "../app.js";
+import config from "../config/index.js";
+import sequelize from "../src/libs/dataBase.js";
+
+async function main() {
+  try {
+    await sequelize.authenticate();
+    console.log("Start DB");
+    await sequelize.sync();
+    const port = config.port;
+    app.set("port", port);
+
+    const server = http.createServer(app);
+    server.listen(port, () => console.log(`Server started on port: ${port}`));
+    server.on("error", (err) => {
+      console.log(err);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+main();
--- src/app.js
+++ src/app.js
@@ -3,12 +3,12 @@ import cors from "cors";
 import helmet from "helmet";
 import express from "express";
 import compression from "compression";
-import routes from "./src/routes/index.js";
+import routes from "./routes/index.js";
 import {
   logErrors,
   wrapErrors,
   errorHandle,
-} from "./src/utils/middlewares/errorMiddleware.js";
+} from "./utils/middlewares/errorMiddleware.js";
 
 const app = express();
 
