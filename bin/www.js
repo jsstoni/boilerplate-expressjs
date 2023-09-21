408c7cfd6c43fa576b435599297e13c3309e37d6
jsstoni <jsstoniha@gmail.com>
2023-09-21 14:02:10 -0300

    remove Sequelize support

--- bin/www.js
+++ bin/www.js
@@ -1,13 +1,9 @@
 import http from "http";
 import app from "../src/app.js";
 import config from "../config/index.js";
-import sequelize from "../src/libs/dataBase.js";
 
-async function main() {
+function main() {
   try {
-    await sequelize.authenticate();
-    console.log("Start DB");
-    await sequelize.sync();
     const port = config.port;
     app.set("port", port);
 
